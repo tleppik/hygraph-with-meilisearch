f66838bc8f710ebedbf955acf76a4f33c6e74c75
Bastian K <bastian.kippelt@d-velop.de>
2023-08-28 23:51:30 +0200

    update upload

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -1,33 +1,62 @@
+import { MeiliSearch } from 'meilisearch'
+import { astToHtmlString } from '@graphcms/rich-text-html-renderer';
+
 export default defineEventHandler(async (event) => {
+
+    // if(event.node.req.headers['Authorization']) {
+    //     console.log("Authorization", event.node.req.headers['Authorization']);
+    // }
 
     if (event.node.req.method === 'GET') {
         throw createError({ statusCode: 405, statusMessage: 'Method not allowed' })
     }
 
     if (event.node.req.method === 'POST') {
-        const body = await readBody(event);
-        try {
 
-            const host: string = process.env.HYGRAPH_API;
-            const token: string = process.env.HYGRAPH_TOKEN;
+        try {
+            const body = await readBody(event);
+            const faq = JSON.parse(body);
+    
+            const locale = faq.data.localizations;
+            const content = locale[0].answer.raw;
+    
+            const answer = astToHtmlString({
+                content,
+            });
+    
+            const document = {
+                id: faq.data.id,
+                created: faq.data.createdAt,
+                answer: answer,
+                question: locale[0].question
+            }
+
+            const meilisearch = {
+                host: process.env.MEILISEARCH_URL,
+                apiKey: process.env.MEILISEARCH_API_KEY,
+                index: process.env.MEILISEARCH_INDEX
+            }
+
+            const client = new MeiliSearch({
+                host: meilisearch.host,
+                apiKey: meilisearch.apiKey,
+            });
+
+            if (client === null) {
+                throw createError({ statusCode: 500, statusMessage: 'meilisearch client is null' })
+            }
+
+            const index = client.index(meilisearch.index);
+            if (client === null) {
+                throw createError({ statusCode: 500, statusMessage: 'meilisearch index is null' })
+            }
 
             // add or replace documents
             // /indexes/{index_uid}/documents
-            const resp = await fetch(host, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Authorization": token
-                },
-                body: body
-            });
-
-            const data = await resp.json();
-            if (data === null) {
-                throw new Error('data is null');
-            }
-
-            return { body }
+            let response = await index.addDocuments([
+                document
+            ]);
+            return response;
 
         } catch (err) {
             throw createError({ statusCode: 403, statusMessage: err.message })
