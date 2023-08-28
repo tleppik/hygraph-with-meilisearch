85226f2e626aa104e64612bc8bf2534e918e1352
Bastian K <bastian.kippelt@d-velop.de>
2023-08-28 22:40:22 +0200

    add webhook logic

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -1,11 +1,36 @@
-export default defineEventHandler(async  (event) => {
-    
+export default defineEventHandler(async (event) => {
+
     if (event.node.req.method === 'GET') {
-        return "wrong type";
+        throw createError({ statusCode: 405, statusMessage: 'Method not allowed' })
     }
 
-    if(event.node.req.method === 'POST') {
-        const body = await readBody(event);       
-        return { body } 
+    if (event.node.req.method === 'POST') {
+        const body = await readBody(event);
+        try {
+
+            const host: string = process.env.HYGRAPH_API;
+            const token: string = process.env.HYGRAPH_TOKEN;
+
+            // add or replace documents
+            // /indexes/{index_uid}/documents
+            const resp = await fetch(host, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": token
+                },
+                body: body
+            });
+
+            const data = await resp.json();
+            if (data === null) {
+                throw new Error('data is null');
+            }
+
+            return { body }
+
+        } catch (err) {
+            throw createError({ statusCode: 403, statusMessage: err.message })
+        }
     }
-  })
+})
