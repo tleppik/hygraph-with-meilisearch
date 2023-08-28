164eae4a36da26ecdd204a12b7926879a712753b
Bastian K <bastian.kippelt@d-velop.de>
2023-08-28 21:53:35 +0200

    Add webhook

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>hygraph with meilisearch</h1>
   </div>
 </template>
---
+++ server/api/webhook.ts
@@ -0,0 +1,11 @@
+export default defineEventHandler((event) => {
+    
+    if (event.node.req.method === 'GET') {
+        return "wrong type";
+    }
+
+    if(event.node.req.method === 'POST') {
+        const body = await readBody(event);       
+        return { body } 
+    }
+  })
